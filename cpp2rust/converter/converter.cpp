3c56d9b3df62a7d868163e10b59d0bc3710f1505
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-09 20:58:21 +0100

    Use null/null_mut for default pointers

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -1849,7 +1849,9 @@ bool Converter::VisitImplicitCastExpr(clang::ImplicitCastExpr *expr) {
     if (type->isFunctionPointerType()) {
       StrCat("None");
     } else {
-      StrCat(keyword_default_);
+      StrCat(type->getPointeeType().isConstQualified()
+                 ? "std::ptr::null()"
+                 : "std::ptr::null_mut()");
     }
     computed_expr_type_ = ComputedExprType::FreshPointer;
     break;
@@ -2953,12 +2955,13 @@ std::string Converter::GetDefaultAsString(clang::QualType qual_type) {
   }
 
   if (qual_type->isPointerType()) {
-    if (qual_type->getPointeeType()->isFunctionType()) {
+    auto pointee = qual_type->getPointeeType();
+    if (pointee->isFunctionType()) {
       return "None";
-    } else {
-      computed_expr_type_ = ComputedExprType::FreshPointer;
-      return keyword_default_;
     }
+    computed_expr_type_ = ComputedExprType::FreshPointer;
+    return pointee.isConstQualified() ? "std::ptr::null()"
+                                      : "std::ptr::null_mut()";
   }
 
   computed_expr_type_ = ComputedExprType::FreshValue;
