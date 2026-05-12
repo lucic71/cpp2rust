9de95ad2e3ae61f60a448012e79df83e81bffc2b
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-12 16:05:52 +0100

    Add rules for socket_type enum values

--- rules/socket/src.cpp
+++ rules/socket/src.cpp
@@ -4,3 +4,39 @@
 int f1() {
   return MSG_NOSIGNAL;
 }
+
+int f2() {
+  return SOCK_STREAM;
+}
+
+int f3() {
+  return SOCK_DGRAM;
+}
+
+int f4() {
+  return SOCK_RAW;
+}
+
+int f5() {
+  return SOCK_RDM;
+}
+
+int f6() {
+  return SOCK_SEQPACKET;
+}
+
+int f7() {
+  return SOCK_DCCP;
+}
+
+int f8() {
+  return SOCK_PACKET;
+}
+
+int f9() {
+  return SOCK_CLOEXEC;
+}
+
+int f10() {
+  return SOCK_NONBLOCK;
+}
--- rules/socket/tgt_unsafe.rs
+++ rules/socket/tgt_unsafe.rs
@@ -1,3 +1,39 @@
 unsafe fn f1() -> i32 {
     libc::MSG_NOSIGNAL
 }
+
+unsafe fn f2() -> i32 {
+    libc::SOCK_STREAM
+}
+
+unsafe fn f3() -> i32 {
+    libc::SOCK_DGRAM
+}
+
+unsafe fn f4() -> i32 {
+    libc::SOCK_RAW
+}
+
+unsafe fn f5() -> i32 {
+    libc::SOCK_RDM
+}
+
+unsafe fn f6() -> i32 {
+    libc::SOCK_SEQPACKET
+}
+
+unsafe fn f7() -> i32 {
+    libc::SOCK_DCCP
+}
+
+unsafe fn f8() -> i32 {
+    libc::SOCK_PACKET
+}
+
+unsafe fn f9() -> i32 {
+    libc::SOCK_CLOEXEC
+}
+
+unsafe fn f10() -> i32 {
+    libc::SOCK_NONBLOCK
+}
