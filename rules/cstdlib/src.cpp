f0faa2f0522e5e27948ba01b3c0bc8cd8181686e
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-13 17:33:34 +0100

    Add rules for malloc, realloc, and free for unsafe model (#91)

--- rules/cstdlib/src.cpp
+++ rules/cstdlib/src.cpp
@@ -4,3 +4,9 @@
 #include <cstdlib>
 
 void f1() { return std::abort(); }
+
+void f2(void *a0) { return free(a0); }
+
+void *f3(size_t a0) { return malloc(a0); }
+
+void *f4(void *a0, size_t a1) { return realloc(a0, a1); }
