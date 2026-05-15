d11f8f57878bc8d16e84567c0bbd958810f1f84e
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-15 17:31:30 +0100

    Add test for implicit conversion in vector ctor

---
+++ tests/unit/out/unsafe/vector_iter_range_ctor.rs
@@ -0,0 +1,53 @@
+extern crate libc;
+use libc::*;
+extern crate libcc2rs;
+use libcc2rs::*;
+use std::collections::BTreeMap;
+use std::io::{Read, Seek, Write};
+use std::os::fd::{AsFd, FromRawFd, IntoRawFd};
+use std::rc::Rc;
+pub fn main() {
+    unsafe {
+        std::process::exit(main_0() as i32);
+    }
+}
+unsafe fn main_0() -> i32 {
+    let mut src: [u32; 3] = [1_u32, 2_u32, 3_u32];
+    let mut v1: Vec<u32> = core::slice::from_raw_parts(
+        src.as_mut_ptr(),
+        (src.as_mut_ptr().offset((3) as isize)).offset_from(src.as_mut_ptr()) as usize,
+    )
+    .iter()
+    .map(|x| u32::try_from(x.clone()).ok().unwrap())
+    .collect();
+    assert!(((v1.len() as u64) == (3_u64)));
+    assert!(
+        (((v1[(0_u64) as usize]) == (1_u32)) && ((v1[(1_u64) as usize]) == (2_u32)))
+            && ((v1[(2_u64) as usize]) == (3_u32))
+    );
+    let mut v2: Vec<u64> = core::slice::from_raw_parts(
+        src.as_mut_ptr(),
+        (src.as_mut_ptr().offset((3) as isize)).offset_from(src.as_mut_ptr()) as usize,
+    )
+    .iter()
+    .map(|x| u64::try_from(x.clone()).ok().unwrap())
+    .collect();
+    assert!(((v2.len() as u64) == (3_u64)));
+    assert!(
+        (((v2[(0_u64) as usize]) == (1_u64)) && ((v2[(1_u64) as usize]) == (2_u64)))
+            && ((v2[(2_u64) as usize]) == (3_u64))
+    );
+    let mut v3: Vec<i32> = core::slice::from_raw_parts(
+        src.as_mut_ptr(),
+        (src.as_mut_ptr().offset((3) as isize)).offset_from(src.as_mut_ptr()) as usize,
+    )
+    .iter()
+    .map(|x| i32::try_from(x.clone()).ok().unwrap())
+    .collect();
+    assert!(((v3.len() as u64) == (3_u64)));
+    assert!(
+        (((v3[(0_u64) as usize]) == (1)) && ((v3[(1_u64) as usize]) == (2)))
+            && ((v3[(2_u64) as usize]) == (3))
+    );
+    return 0;
+}
