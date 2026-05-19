46ae019c77773acea2615d2a97c8109cf15bbdfc
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-19 15:14:26 +0100

    Delete double define _GNU_SOURCE
    It's already defined in src.c

--- rules/cstring/src.cpp
+++ rules/cstring/src.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) 2022-present INESC-ID.
 // Distributed under the MIT license that can be found in the LICENSE file.
 
-#define _GNU_SOURCE
 #include <string.h>
 
 const char *f6(const char *a0, int a1) { return strchr(a0, a1); }
