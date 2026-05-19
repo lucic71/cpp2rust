1bef431bd20c33678cd0baa4c27c3a1a7171db9e
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-19 13:50:31 +0100

    Add unistd rules

---
+++ tests/unit/out/unsafe/unistd.rs
@@ -0,0 +1,530 @@
+extern crate libc;
+use libc::*;
+extern crate libcc2rs;
+use libcc2rs::*;
+use std::collections::BTreeMap;
+use std::io::{Read, Seek, Write};
+use std::os::fd::{AsFd, FromRawFd, IntoRawFd};
+use std::rc::Rc;
+pub unsafe fn test_close_0() {
+    let mut fds: [i32; 2] = [0_i32; 2];
+    assert!(((((libc::pipe(fds.as_mut_ptr())) == (0)) as i32) != 0));
+    assert!(((((libc::close(fds[(0) as usize])) == (0)) as i32) != 0));
+    let mut buf: [u8; 1] = [0_u8; 1];
+    assert!(
+        ((((libc::read(
+            fds[(0) as usize],
+            (buf.as_mut_ptr() as *mut u8 as *mut ::libc::c_void),
+            1_u64 as usize
+        ) as i64)
+            == (-1_i32 as i64)) as i32)
+            != 0)
+    );
+    assert!(((((libc::close(fds[(1) as usize])) == (0)) as i32) != 0));
+}
+pub unsafe fn test_lseek_1() {
+    let mut path: *const u8 = (b"/tmp/cpp2rust_lseek_test.tmp\0".as_ptr().cast_mut()).cast_const();
+    let mut fp: *mut ::std::fs::File =
+        match std::ffi::CStr::from_ptr((b"wb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+            .to_str()
+            .expect("invalid c-string")
+        {
+            v if v == "rb" => std::fs::OpenOptions::new()
+                .read(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            v if v == "wb" => std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            _ => panic!("unsupported mode"),
+        };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    {
+        let bytes = std::ffi::CStr::from_ptr(
+            (b"hello world\0".as_ptr().cast_mut()).cast_const() as *const i8
+        )
+        .to_bytes();
+        match (*fp).write_all(bytes) {
+            Ok(()) => 0,
+            Err(_) => -1,
+        }
+    };
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    fp = match std::ffi::CStr::from_ptr((b"rb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+        .to_str()
+        .expect("invalid c-string")
+    {
+        v if v == "rb" => std::fs::OpenOptions::new()
+            .read(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        v if v == "wb" => std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        _ => panic!("unsupported mode"),
+    };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    let mut fd: i32 = if fp == libcc2rs::cin_unsafe() {
+        0
+    } else if fp == libcc2rs::cout_unsafe() {
+        1
+    } else if fp == libcc2rs::cerr_unsafe() {
+        2
+    } else {
+        ::std::os::fd::AsRawFd::as_raw_fd(&*fp)
+    };
+    assert!(((((libc::lseek(fd, 0_i64, 2)) == (11_i64)) as i32) != 0));
+    assert!(((((libc::lseek(fd, 6_i64, 0)) == (6_i64)) as i32) != 0));
+    let mut buf: [u8; 8] = [0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8];
+    assert!(
+        ((((libc::read(
+            fd,
+            (buf.as_mut_ptr() as *mut u8 as *mut ::libc::c_void),
+            5_u64 as usize
+        ) as i64)
+            == (5_i64)) as i32)
+            != 0)
+    );
+    assert!(
+        (((({
+            let sa = core::slice::from_raw_parts(
+                (buf.as_mut_ptr() as *const u8 as *const ::libc::c_void) as *const u8,
+                5_u64 as usize,
+            );
+            let sb = core::slice::from_raw_parts(
+                (b"world\0".as_ptr().cast_mut() as *const u8 as *const ::libc::c_void) as *const u8,
+                5_u64 as usize,
+            );
+            let mut diff = 0_i32;
+            for (x, y) in sa.iter().zip(sb.iter()) {
+                if x != y {
+                    diff = (*x as i32) - (*y as i32);
+                    break;
+                }
+            }
+            diff
+        }) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    libc::unlink(path as *const i8);
+}
+pub unsafe fn test_read_2() {
+    let mut path: *const u8 = (b"/tmp/cpp2rust_read_test.tmp\0".as_ptr().cast_mut()).cast_const();
+    let mut fp: *mut ::std::fs::File =
+        match std::ffi::CStr::from_ptr((b"wb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+            .to_str()
+            .expect("invalid c-string")
+        {
+            v if v == "rb" => std::fs::OpenOptions::new()
+                .read(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            v if v == "wb" => std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            _ => panic!("unsupported mode"),
+        };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    {
+        let bytes = std::ffi::CStr::from_ptr(
+            (b"hello world\0".as_ptr().cast_mut()).cast_const() as *const i8
+        )
+        .to_bytes();
+        match (*fp).write_all(bytes) {
+            Ok(()) => 0,
+            Err(_) => -1,
+        }
+    };
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    fp = match std::ffi::CStr::from_ptr((b"rb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+        .to_str()
+        .expect("invalid c-string")
+    {
+        v if v == "rb" => std::fs::OpenOptions::new()
+            .read(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        v if v == "wb" => std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        _ => panic!("unsupported mode"),
+    };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    let mut fd: i32 = if fp == libcc2rs::cin_unsafe() {
+        0
+    } else if fp == libcc2rs::cout_unsafe() {
+        1
+    } else if fp == libcc2rs::cerr_unsafe() {
+        2
+    } else {
+        ::std::os::fd::AsRawFd::as_raw_fd(&*fp)
+    };
+    let mut buf: [u8; 16] = [
+        0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8,
+        0_u8,
+    ];
+    assert!(
+        ((((libc::read(
+            fd,
+            (buf.as_mut_ptr() as *mut u8 as *mut ::libc::c_void),
+            16_u64 as usize
+        ) as i64)
+            == (11_i64)) as i32)
+            != 0)
+    );
+    assert!(
+        (((({
+            let sa = core::slice::from_raw_parts(
+                (buf.as_mut_ptr() as *const u8 as *const ::libc::c_void) as *const u8,
+                11_u64 as usize,
+            );
+            let sb = core::slice::from_raw_parts(
+                (b"hello world\0".as_ptr().cast_mut() as *const u8 as *const ::libc::c_void)
+                    as *const u8,
+                11_u64 as usize,
+            );
+            let mut diff = 0_i32;
+            for (x, y) in sa.iter().zip(sb.iter()) {
+                if x != y {
+                    diff = (*x as i32) - (*y as i32);
+                    break;
+                }
+            }
+            diff
+        }) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    libc::unlink(path as *const i8);
+}
+pub unsafe fn test_unlink_3() {
+    let mut path: *const u8 = (b"/tmp/cpp2rust_unlink_test.tmp\0".as_ptr().cast_mut()).cast_const();
+    let mut fp: *mut ::std::fs::File =
+        match std::ffi::CStr::from_ptr((b"wb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+            .to_str()
+            .expect("invalid c-string")
+        {
+            v if v == "rb" => std::fs::OpenOptions::new()
+                .read(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            v if v == "wb" => std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            _ => panic!("unsupported mode"),
+        };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    assert!(((((libc::unlink(path as *const i8)) == (0)) as i32) != 0));
+    assert!(((((libc::unlink(path as *const i8)) == (-1_i32)) as i32) != 0));
+}
+pub unsafe fn test_pipe_4() {
+    let mut fds: [i32; 2] = [0_i32; 2];
+    assert!(((((libc::pipe(fds.as_mut_ptr())) == (0)) as i32) != 0));
+    let mut msg: *const u8 = (b"world\0".as_ptr().cast_mut()).cast_const();
+    assert!(
+        ((((libc::write(
+            fds[(1) as usize],
+            (msg as *const u8 as *const ::libc::c_void),
+            5_u64 as usize
+        ) as i64)
+            == (5_i64)) as i32)
+            != 0)
+    );
+    let mut buf: [u8; 8] = [0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8];
+    assert!(
+        ((((libc::read(
+            fds[(0) as usize],
+            (buf.as_mut_ptr() as *mut u8 as *mut ::libc::c_void),
+            8_u64 as usize
+        ) as i64)
+            == (5_i64)) as i32)
+            != 0)
+    );
+    assert!(
+        (((({
+            let sa = core::slice::from_raw_parts(
+                (buf.as_mut_ptr() as *const u8 as *const ::libc::c_void) as *const u8,
+                5_u64 as usize,
+            );
+            let sb = core::slice::from_raw_parts(
+                (msg as *const u8 as *const ::libc::c_void) as *const u8,
+                5_u64 as usize,
+            );
+            let mut diff = 0_i32;
+            for (x, y) in sa.iter().zip(sb.iter()) {
+                if x != y {
+                    diff = (*x as i32) - (*y as i32);
+                    break;
+                }
+            }
+            diff
+        }) == (0)) as i32)
+            != 0)
+    );
+    assert!(((((libc::close(fds[(1) as usize])) == (0)) as i32) != 0));
+    assert!(
+        ((((libc::read(
+            fds[(0) as usize],
+            (buf.as_mut_ptr() as *mut u8 as *mut ::libc::c_void),
+            8_u64 as usize
+        ) as i64)
+            == (0_i64)) as i32)
+            != 0)
+    );
+    assert!(((((libc::close(fds[(0) as usize])) == (0)) as i32) != 0));
+}
+pub unsafe fn test_ftruncate_5() {
+    let mut path: *const u8 =
+        (b"/tmp/cpp2rust_ftruncate_test.tmp\0".as_ptr().cast_mut()).cast_const();
+    let mut fp: *mut ::std::fs::File =
+        match std::ffi::CStr::from_ptr((b"wb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+            .to_str()
+            .expect("invalid c-string")
+        {
+            v if v == "rb" => std::fs::OpenOptions::new()
+                .read(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            v if v == "wb" => std::fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(
+                    std::ffi::CStr::from_ptr(path as *const i8)
+                        .to_str()
+                        .expect("invalid c-string"),
+                )
+                .ok()
+                .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+            _ => panic!("unsupported mode"),
+        };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    {
+        let bytes = std::ffi::CStr::from_ptr(
+            (b"hello world\0".as_ptr().cast_mut()).cast_const() as *const i8
+        )
+        .to_bytes();
+        match (*fp).write_all(bytes) {
+            Ok(()) => 0,
+            Err(_) => -1,
+        }
+    };
+    if !(fp).is_null() {
+        match (*fp).sync_all() {
+            Ok(_) => 0,
+            Err(_) => -1,
+        }
+    } else {
+        ::std::io::stdout().flush().unwrap();
+        ::std::io::stderr().flush().unwrap();
+        0
+    };
+    let mut fd: i32 = if fp == libcc2rs::cin_unsafe() {
+        0
+    } else if fp == libcc2rs::cout_unsafe() {
+        1
+    } else if fp == libcc2rs::cerr_unsafe() {
+        2
+    } else {
+        ::std::os::fd::AsRawFd::as_raw_fd(&*fp)
+    };
+    assert!(((((libc::ftruncate(fd, 5_i64)) == (0)) as i32) != 0));
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    fp = match std::ffi::CStr::from_ptr((b"rb\0".as_ptr().cast_mut()).cast_const() as *const i8)
+        .to_str()
+        .expect("invalid c-string")
+    {
+        v if v == "rb" => std::fs::OpenOptions::new()
+            .read(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        v if v == "wb" => std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(
+                std::ffi::CStr::from_ptr(path as *const i8)
+                    .to_str()
+                    .expect("invalid c-string"),
+            )
+            .ok()
+            .map_or(std::ptr::null_mut(), |f| Box::into_raw(Box::new(f))),
+        _ => panic!("unsupported mode"),
+    };
+    assert!((((!((fp).is_null())) as i32) != 0));
+    fd = (if fp == libcc2rs::cin_unsafe() {
+        0
+    } else if fp == libcc2rs::cout_unsafe() {
+        1
+    } else if fp == libcc2rs::cerr_unsafe() {
+        2
+    } else {
+        ::std::os::fd::AsRawFd::as_raw_fd(&*fp)
+    })
+    .clone();
+    assert!(((((libc::lseek(fd, 0_i64, 2)) == (5_i64)) as i32) != 0));
+    assert!(
+        (((({
+            Box::from_raw(fp);
+            0
+        }) == (0)) as i32)
+            != 0)
+    );
+    libc::unlink(path as *const i8);
+}
+pub unsafe fn test_isatty_6() {
+    printf(
+        (b"%d\n\0".as_ptr().cast_mut()).cast_const() as *const i8,
+        libc::isatty(0),
+    );
+}
+pub unsafe fn test_geteuid_7() {
+    printf(
+        (b"%u\n\0".as_ptr().cast_mut()).cast_const() as *const i8,
+        libc::geteuid(),
+    );
+}
+pub unsafe fn test_gethostname_8() {
+    let mut name: [u8; 256] = [0_u8; 256];
+    assert!(
+        ((((libc::gethostname(
+            name.as_mut_ptr() as *mut i8,
+            ::std::mem::size_of::<[u8; 256]>() as u64 as usize
+        )) == (0)) as i32)
+            != 0)
+    );
+    printf(
+        (b"%s\n\0".as_ptr().cast_mut()).cast_const() as *const i8,
+        name.as_mut_ptr(),
+    );
+}
+pub fn main() {
+    unsafe {
+        std::process::exit(main_0() as i32);
+    }
+}
+unsafe fn main_0() -> i32 {
+    (unsafe { test_close_0() });
+    (unsafe { test_lseek_1() });
+    (unsafe { test_read_2() });
+    (unsafe { test_unlink_3() });
+    (unsafe { test_pipe_4() });
+    (unsafe { test_ftruncate_5() });
+    (unsafe { test_isatty_6() });
+    (unsafe { test_geteuid_7() });
+    (unsafe { test_gethostname_8() });
+    return 0;
+}
---
+++ tests/unit/unistd.c
@@ -0,0 +1,115 @@
+// no-compile: refcount
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static void test_close(void) {
+  int fds[2];
+  assert(pipe(fds) == 0);
+  assert(close(fds[0]) == 0);
+  char buf[1];
+  assert(read(fds[0], buf, 1) == -1);
+  assert(close(fds[1]) == 0);
+}
+
+static void test_lseek(void) {
+  const char *path = "/tmp/cpp2rust_lseek_test.tmp";
+  FILE *fp = fopen(path, "wb");
+  assert(fp != NULL);
+  fputs("hello world", fp);
+  assert(fclose(fp) == 0);
+  fp = fopen(path, "rb");
+  assert(fp != NULL);
+  int fd = fileno(fp);
+  assert(lseek(fd, 0, SEEK_END) == 11);
+  assert(lseek(fd, 6, SEEK_SET) == 6);
+  char buf[8] = {0};
+  assert(read(fd, buf, 5) == 5);
+  assert(memcmp(buf, "world", 5) == 0);
+  assert(fclose(fp) == 0);
+  unlink(path);
+}
+
+static void test_read(void) {
+  const char *path = "/tmp/cpp2rust_read_test.tmp";
+  FILE *fp = fopen(path, "wb");
+  assert(fp != NULL);
+  fputs("hello world", fp);
+  assert(fclose(fp) == 0);
+  fp = fopen(path, "rb");
+  assert(fp != NULL);
+  int fd = fileno(fp);
+  char buf[16] = {0};
+  assert(read(fd, buf, 16) == 11);
+  assert(memcmp(buf, "hello world", 11) == 0);
+  assert(fclose(fp) == 0);
+  unlink(path);
+}
+
+static void test_unlink(void) {
+  const char *path = "/tmp/cpp2rust_unlink_test.tmp";
+  FILE *fp = fopen(path, "wb");
+  assert(fp != NULL);
+  assert(fclose(fp) == 0);
+  assert(unlink(path) == 0);
+  assert(unlink(path) == -1);
+}
+
+static void test_pipe(void) {
+  int fds[2];
+  assert(pipe(fds) == 0);
+  const char *msg = "world";
+  assert(write(fds[1], msg, 5) == 5);
+  char buf[8] = {0};
+  assert(read(fds[0], buf, 8) == 5);
+  assert(memcmp(buf, msg, 5) == 0);
+  assert(close(fds[1]) == 0);
+  assert(read(fds[0], buf, 8) == 0);
+  assert(close(fds[0]) == 0);
+}
+
+static void test_ftruncate(void) {
+  const char *path = "/tmp/cpp2rust_ftruncate_test.tmp";
+  FILE *fp = fopen(path, "wb");
+  assert(fp != NULL);
+  fputs("hello world", fp);
+  fflush(fp);
+  int fd = fileno(fp);
+  assert(ftruncate(fd, 5) == 0);
+  assert(fclose(fp) == 0);
+  fp = fopen(path, "rb");
+  assert(fp != NULL);
+  fd = fileno(fp);
+  assert(lseek(fd, 0, SEEK_END) == 5);
+  assert(fclose(fp) == 0);
+  unlink(path);
+}
+
+static void test_isatty(void) {
+  printf("%d\n", isatty(0));
+}
+
+static void test_geteuid(void) {
+  printf("%u\n", geteuid());
+}
+
+static void test_gethostname(void) {
+  char name[256];
+  assert(gethostname(name, sizeof(name)) == 0);
+  printf("%s\n", name);
+}
+
+int main(void) {
+  test_close();
+  test_lseek();
+  test_read();
+  test_unlink();
+  test_pipe();
+  test_ftruncate();
+  test_isatty();
+  test_geteuid();
+  test_gethostname();
+  return 0;
+}
