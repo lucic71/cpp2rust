686e19ef69f3f2e2d26c6fca823198eb87f0f719
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-25 19:43:43 +0100

    Add rules for inet_ntop and inet_pton (#148)
    inet_ntop and inet_pton don't have mappings in Rust libc. I declared
    them extern because they exist in the linked-against libc.

--- rules/arpa_inet/tgt_unsafe.rs
+++ rules/arpa_inet/tgt_unsafe.rs
@@ -13,3 +13,15 @@ unsafe fn f3(a0: u16) -> u16 {
 unsafe fn f4(a0: u32) -> u32 {
     u32::to_be(a0)
 }
+unsafe fn f5(a0: i32, a1: *const u8, a2: *mut ::libc::c_void) -> i32 {
+    unsafe extern "C" {
+        fn inet_pton(af: i32, src: *const u8, dst: *mut ::libc::c_void) -> i32;
+    }
+    inet_pton(a0, a1, a2)
+}
+unsafe fn f6(a0: i32, a1: *const ::libc::c_void, a2: *mut u8, a3: u32) -> *const u8 {
+    unsafe extern "C" {
+        fn inet_ntop(af: i32, src: *const ::libc::c_void, dst: *mut u8, size: u32) -> *const u8;
+    }
+    inet_ntop(a0, a1, a2, a3)
+}
