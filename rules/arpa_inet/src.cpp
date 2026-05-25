e3986cd8406f413828952c2bf6e22067cfaf4caa
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-25 13:29:32 +0100

    Add rules for inet_ntop and inet_pton

--- rules/arpa_inet/src.cpp
+++ rules/arpa_inet/src.cpp
@@ -7,3 +7,7 @@ uint32_t f1(uint32_t x) { return ntohl(x); }
 uint16_t f2(uint16_t x) { return ntohs(x); }
 uint16_t f3(uint16_t x) { return htons(x); }
 uint32_t f4(uint32_t x) { return htonl(x); }
+int f5(int af, const char *src, void *dst) { return inet_pton(af, src, dst); }
+const char *f6(int af, const void *src, char *dst, socklen_t size) {
+  return inet_ntop(af, src, dst, size);
+}
