32ffacfd35f5b01e0874361c7b0890391c7fb573
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2026-06-08 09:19:48 +0100

    move benchmarks to the testsuite repo

--- tests/benchmarks/fibonacci.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022-present INESC-ID.
-// Distributed under the MIT license that can be found in the LICENSE file.
-
-unsigned long long fib(unsigned long long n) {
-  return n == 0 || n == 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-int main() { return fib(46); }
