4f8a31c9cce9326d0d1bf734e8002c7d5ac33d3c
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-07 11:00:23 +0100

    clang format

--- tests/unit/pointer_add_assign.cpp
+++ tests/unit/pointer_add_assign.cpp
@@ -8,7 +8,7 @@ int main() {
     p += static_cast<uint8_t>(1);
     assert(*p == 2);
   }
-  
+
   {
     char *p = &arr[0];
     p += static_cast<int8_t>(1);
@@ -20,7 +20,7 @@ int main() {
     p += static_cast<uint16_t>(1);
     assert(*p == 2);
   }
-  
+
   {
     char *p = &arr[0];
     p += static_cast<int16_t>(1);
@@ -32,7 +32,7 @@ int main() {
     p += static_cast<uint32_t>(1);
     assert(*p == 2);
   }
-  
+
   {
     char *p = &arr[0];
     p += static_cast<int32_t>(1);
@@ -44,7 +44,7 @@ int main() {
     p += static_cast<uint64_t>(1);
     assert(*p == 2);
   }
-  
+
   {
     char *p = &arr[0];
     p += static_cast<int64_t>(1);
