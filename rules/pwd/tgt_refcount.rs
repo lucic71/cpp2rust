4f6bc2fe50a2e7f710701c9582c96c175e7fc41a
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-18 13:07:53 +0100

    Set errno on getpwuid error

--- rules/pwd/tgt_refcount.rs
+++ rules/pwd/tgt_refcount.rs
@@ -10,6 +10,10 @@ fn t1() -> libcc2rs::Passwd {
 fn f1(a0: u32) -> Ptr<Passwd> {
     match nix::unistd::User::from_uid(nix::unistd::Uid::from_raw(a0)) {
         Ok(Some(__u)) => Ptr::alloc(Passwd::from_user(&__u)),
-        _ => Ptr::null(),
+        Ok(None) => Ptr::null(),
+        Err(__e) => {
+            libcc2rs::cpp2rust_errno().write(__e as i32);
+            Ptr::null()
+        }
     }
 }
