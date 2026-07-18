20496f0bad32b7b3188e5f0631433241921e455c
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-18 14:35:50 +0100

    Add localtime_r safe rule

--- tests/unit/sys_time.c
+++ tests/unit/sys_time.c
@@ -34,6 +34,22 @@ static void test_gmtime_r(void) {
   print_tm(4102444800);
 }
 
+static void print_local_tm(time_t t) {
+  struct tm tm;
+  assert(localtime_r(&t, &tm) != NULL);
+  printf("%d-%d-%d %d:%d:%d wday=%d yday=%d %s gmtoff=%ld isdst=%d\n",
+         tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,
+         tm.tm_wday, tm.tm_yday, tm.tm_zone, tm.tm_gmtoff, tm.tm_isdst);
+}
+
+static void test_localtime_r(void) {
+  print_local_tm(0);
+  print_local_tm(951782400);
+  print_local_tm(1704067199);
+  print_local_tm(1721126096);
+  print_local_tm(1735689600);
+}
+
 static void test_strftime(void) {
   time_t t = 1721126096;
   struct tm tm;
@@ -54,6 +70,7 @@ static void test_strftime(void) {
 int main(void) {
   test_time();
   test_gmtime_r();
+  test_localtime_r();
   test_strftime();
   return 0;
 }
