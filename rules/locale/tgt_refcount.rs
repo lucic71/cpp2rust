f808c20bb54203daf0873524ca3091899e7d828b
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-18 14:11:54 +0100

    Add localization todo

--- rules/locale/tgt_refcount.rs
+++ rules/locale/tgt_refcount.rs
@@ -3,6 +3,10 @@
 
 use libcc2rs::*;
 
+// Rust does not have support for localization.
+//
+// TODO: we need to track ourselves the locale settings and change the behavior of the relevant
+// functions based on the set locale.
 fn f1(a0: i32, a1: Ptr<u8>) -> Ptr<u8> {
     Ptr::from_string_literal(b"C")
 }
