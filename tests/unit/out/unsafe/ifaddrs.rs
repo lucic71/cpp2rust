a89ebadb0d789d00bd3f8ca564fea7567558ccaa
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-18 16:00:10 +0100

    Add if_nametoindex safe rule

--- tests/unit/out/unsafe/ifaddrs.rs
+++ tests/unit/out/unsafe/ifaddrs.rs
@@ -79,10 +79,20 @@ unsafe fn main_0() -> i32 {
                 }) == (0)) as i32)
                     != 0)
             );
+            assert!(
+                ((((libc::if_nametoindex(((*ifa).ifa_name).cast_const())) > (0_u32)) as i32) != 0)
+            );
         }
         ifa = (*ifa).ifa_next;
     }
     assert!((found_loopback != 0));
     libc::freeifaddrs(list);
+    (*libcc2rs::cpp2rust_errno_unsafe()) = 0;
+    assert!(
+        ((((libc::if_nametoindex((c"cpp2rust_no_such_if".as_ptr().cast_mut()).cast_const()))
+            == (0_u32)) as i32)
+            != 0)
+    );
+    assert!(((((*libcc2rs::cpp2rust_errno_unsafe()) == (19)) as i32) != 0));
     return 0;
 }
