23a43adb9ac33a764d6d55c02ebc304d61ec524b
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-20 17:55:21 +0100

    Add fstat and termios safe rules

--- rules/stat/tgt_refcount.rs
+++ rules/stat/tgt_refcount.rs
@@ -20,6 +20,19 @@ fn f1(a0: Ptr<u8>, a1: Ptr<Stat>) -> i32 {
     }
 }
 
+fn f2(a0: i32, a1: Ptr<Stat>) -> i32 {
+    match FdRegistry::with_fd(a0, |__fd| nix::sys::stat::fstat(__fd)) {
+        Ok(__s) => {
+            a1.with_mut(|__st| *__st = Stat::from_libc(&__s));
+            0
+        }
+        Err(__e) => {
+            libcc2rs::cpp2rust_errno().write(__e as i32);
+            -1
+        }
+    }
+}
+
 fn f3(a0: Ptr<u8>, a1: ::libc::mode_t) -> i32 {
     match nix::unistd::mkdir(
         a0.to_rust_string().as_str(),
