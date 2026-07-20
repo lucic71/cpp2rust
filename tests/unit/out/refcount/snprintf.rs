48556107c2184a9f3a3f26493fa4db97fa4fbd2b
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-20 15:08:35 +0100

    Update tests

--- tests/unit/out/refcount/snprintf.rs
+++ tests/unit/out/refcount/snprintf.rs
@@ -14,19 +14,21 @@ fn main_0() -> i32 {
         (0..32).map(|_| <u8>::default()).collect::<Box<[u8]>>(),
     ));
     assert!(
-        (((({
-            let __s = format!("x={} y={}", -3_i32, 7_u32);
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"x=%d y=%u").to_rust_string(),
+                &[(-3_i32).into(), (7_u32).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (::std::mem::size_of::<[u8; 32]>()) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 8) as i32)
+        } == 8) as i32)
             != 0)
     );
     assert!(
@@ -47,19 +49,21 @@ fn main_0() -> i32 {
             != 0)
     );
     assert!(
-        (((({
-            let __s = format!("{}", Ptr::from_string_literal(b"hello"));
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%s").to_rust_string(),
+                &[(Ptr::from_string_literal(b"hello")).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (4_usize) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if 4_usize > 0 {
+                let __n = ::std::cmp::min(__b.len(), 4_usize - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 5) as i32)
+        } == 5) as i32)
             != 0)
     );
     assert!(
@@ -80,25 +84,27 @@ fn main_0() -> i32 {
             != 0)
     );
     assert!(
-        (((({
-            let __s = format!("{:05}|{:02x}", 42, 255);
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%05d|%x|%X").to_rust_string(),
+                &[(42).into(), (255).into(), (255).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (::std::mem::size_of::<[u8; 32]>()) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 8) as i32)
+        } == 11) as i32)
             != 0)
     );
     assert!(
         ((({
             let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
-            let mut __it2 = Ptr::from_string_literal(b"00042|ff").to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"00042|ff|FF").to_c_string_iterator();
             loop {
                 let __c1 = __it1.next();
                 let __c2 = __it2.next();
@@ -113,19 +119,161 @@ fn main_0() -> i32 {
             != 0)
     );
     assert!(
-        (((({
-            let __s = format!("{}%", 65 as u8 as char);
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%.2f").to_rust_string(),
+                &[(3.14159E+0).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (::std::mem::size_of::<[u8; 32]>()) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 2) as i32)
+        } == 4) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"3.14").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%-6s|").to_rust_string(),
+                &[(Ptr::from_string_literal(b"ab")).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 7) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"ab    |").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%*d").to_rust_string(),
+                &[(4).into(), (7).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 4) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"   7").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%.3s").to_rust_string(),
+                &[(Ptr::from_string_literal(b"abcdef")).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 3) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"abc").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%c%%").to_rust_string(),
+                &[(65).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 2) as i32)
             != 0)
     );
     assert!(
@@ -146,19 +294,56 @@ fn main_0() -> i32 {
             != 0)
     );
     assert!(
-        (((({
-            let __s = format!("{} {} {}", -1_i64, 1_u64, (9 as usize));
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%+d % d").to_rust_string(),
+                &[(5).into(), (5).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (::std::mem::size_of::<[u8; 32]>()) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 6) as i32)
+        } == 5) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"+5  5").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%ld %lu %zu").to_rust_string(),
+                &[(-1_i64).into(), (1_u64).into(), (9 as usize).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 6) as i32)
             != 0)
     );
     assert!(
@@ -178,23 +363,139 @@ fn main_0() -> i32 {
         } == 0) as i32)
             != 0)
     );
-    (*buf.borrow_mut())[(0) as usize] = (('Z' as i32) as u8);
     assert!(
-        (((({
-            let __s = format!("{}", 123);
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%e").to_rust_string(),
+                &[(1.2345678E+3).into()],
+            );
             let __b = __s.as_bytes();
-            let __size = (0_usize) as usize;
-            if __size > 0 {
-                let __n = ::std::cmp::min(__b.len(), __size - 1);
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
                 (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
                     __dst[..__n].copy_from_slice(&__b[..__n]);
                     __dst[__n] = 0;
                 });
             }
             __b.len() as i32
-        }) == 3) as i32)
+        } == 12) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"1.234568e+03").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%g").to_rust_string(),
+                &[(1.234567E+6).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 11) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"1.23457e+06").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
+    (*buf.borrow_mut())[(0) as usize] = (('Z' as i32) as u8);
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &Ptr::from_string_literal(b"%d").to_rust_string(),
+                &[(123).into()],
+            );
+            let __b = __s.as_bytes();
+            if 0_usize > 0 {
+                let __n = ::std::cmp::min(__b.len(), 0_usize - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 3) as i32)
             != 0)
     );
     assert!((((((*buf.borrow())[(0) as usize] as i32) == ('Z' as i32)) as i32) != 0));
+    let fmt: Value<Box<[u8]>> = Rc::new(RefCell::new(
+        (0..8).map(|_| <u8>::default()).collect::<Box<[u8]>>(),
+    ));
+    (*fmt.borrow_mut())[(0) as usize] = (('%' as i32) as u8);
+    (*fmt.borrow_mut())[(1) as usize] = (('5' as i32) as u8);
+    (*fmt.borrow_mut())[(2) as usize] = (('.' as i32) as u8);
+    (*fmt.borrow_mut())[(3) as usize] = (('1' as i32) as u8);
+    (*fmt.borrow_mut())[(4) as usize] = (('f' as i32) as u8);
+    (*fmt.borrow_mut())[(5) as usize] = 0_u8;
+    assert!(
+        ((({
+            let __s = libcc2rs::format_c(
+                &(fmt.as_pointer() as Ptr<u8>).to_rust_string(),
+                &[(3.26E+0).into()],
+            );
+            let __b = __s.as_bytes();
+            if ::std::mem::size_of::<[u8; 32]>() > 0 {
+                let __n = ::std::cmp::min(__b.len(), ::std::mem::size_of::<[u8; 32]>() - 1);
+                (buf.as_pointer() as Ptr<u8>).with_slice_mut(__n + 1, |__dst| {
+                    __dst[..__n].copy_from_slice(&__b[..__n]);
+                    __dst[__n] = 0;
+                });
+            }
+            __b.len() as i32
+        } == 5) as i32)
+            != 0)
+    );
+    assert!(
+        ((({
+            let mut __it1 = (buf.as_pointer() as Ptr<u8>).to_c_string_iterator();
+            let mut __it2 = Ptr::from_string_literal(b"  3.3").to_c_string_iterator();
+            loop {
+                let __c1 = __it1.next();
+                let __c2 = __it2.next();
+                if __c1 != __c2 {
+                    break (__c1.unwrap_or(0) as i32) - (__c2.unwrap_or(0) as i32);
+                }
+                if __c1.is_none() {
+                    break 0;
+                }
+            }
+        } == 0) as i32)
+            != 0)
+    );
     return 0;
 }
--- tests/unit/out/unsafe/snprintf.rs
+++ tests/unit/out/unsafe/snprintf.rs
@@ -55,17 +55,91 @@ unsafe fn main_0() -> i32 {
             libc::snprintf(
                 buf.as_mut_ptr() as *mut libc::c_char,
                 ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
-                (c"%05d|%02x".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (c"%05d|%x|%X".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
                 (42),
                 (255),
+                (255),
             )
-        }) == (8)) as i32)
+        }) == (11)) as i32)
             != 0)
     );
     assert!(
         ((((libc::strcmp(
             (buf.as_mut_ptr()).cast_const(),
-            (c"00042|ff".as_ptr().cast_mut()).cast_const()
+            (c"00042|ff|FF".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%.2f".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (3.14159E+0),
+            )
+        }) == (4)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"3.14".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%-6s|".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (c"ab".as_ptr().cast_mut()),
+            )
+        }) == (7)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"ab    |".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%*d".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (4),
+                (7),
+            )
+        }) == (4)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"   7".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%.3s".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (c"abcdef".as_ptr().cast_mut()),
+            )
+        }) == (3)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"abc".as_ptr().cast_mut()).cast_const()
         )) == (0)) as i32)
             != 0)
     );
@@ -92,6 +166,25 @@ unsafe fn main_0() -> i32 {
             libc::snprintf(
                 buf.as_mut_ptr() as *mut libc::c_char,
                 ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%+d % d".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (5),
+                (5),
+            )
+        }) == (5)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"+5  5".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
                 (c"%ld %lu %zu".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
                 (-1_i64),
                 (1_u64),
@@ -104,6 +197,42 @@ unsafe fn main_0() -> i32 {
         ((((libc::strcmp(
             (buf.as_mut_ptr()).cast_const(),
             (c"-1 1 9".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%e".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (1.2345678E+3),
+            )
+        }) == (12)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"1.234568e+03".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (c"%g".as_ptr().cast_mut()).cast_const() as *const libc::c_char,
+                (1.234567E+6),
+            )
+        }) == (11)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"1.23457e+06".as_ptr().cast_mut()).cast_const()
         )) == (0)) as i32)
             != 0)
     );
@@ -120,5 +249,30 @@ unsafe fn main_0() -> i32 {
             != 0)
     );
     assert!(((((buf[(0) as usize] as i32) == ('Z' as i32)) as i32) != 0));
+    let mut fmt: [libc::c_char; 8] = [(0 as libc::c_char); 8];
+    fmt[(0) as usize] = (('%' as i32) as libc::c_char);
+    fmt[(1) as usize] = (('5' as i32) as libc::c_char);
+    fmt[(2) as usize] = (('.' as i32) as libc::c_char);
+    fmt[(3) as usize] = (('1' as i32) as libc::c_char);
+    fmt[(4) as usize] = (('f' as i32) as libc::c_char);
+    fmt[(5) as usize] = (0 as libc::c_char);
+    assert!(
+        ((((unsafe {
+            libc::snprintf(
+                buf.as_mut_ptr() as *mut libc::c_char,
+                ::std::mem::size_of::<[libc::c_char; 32]>() as usize,
+                (fmt.as_mut_ptr()).cast_const() as *const libc::c_char,
+                (3.26E+0),
+            )
+        }) == (5)) as i32)
+            != 0)
+    );
+    assert!(
+        ((((libc::strcmp(
+            (buf.as_mut_ptr()).cast_const(),
+            (c"  3.3".as_ptr().cast_mut()).cast_const()
+        )) == (0)) as i32)
+            != 0)
+    );
     return 0;
 }
