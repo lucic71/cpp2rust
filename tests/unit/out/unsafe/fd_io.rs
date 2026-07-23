9a72cbae8a855cd614211875db32df96cdb53583
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-07-23 14:43:18 +0100

    Update tests

--- tests/unit/out/unsafe/fd_io.rs
+++ tests/unit/out/unsafe/fd_io.rs
@@ -66,51 +66,5 @@ unsafe fn main_0() -> i32 {
     );
     assert!(((((libc::close(fd)) == (0)) as i32) != 0));
     assert!(((((libc::unlink(path)) == (0)) as i32) != 0));
-    let mut fds: [i32; 2] = [0_i32; 2];
-    assert!(((((libc::pipe(fds.as_mut_ptr())) == (0)) as i32) != 0));
-    assert!(
-        ((((libc::write(
-            fds[(1) as usize],
-            (c"ab".as_ptr().cast_mut() as *const libc::c_char as *const ::libc::c_void),
-            2_usize
-        )) == (2_isize)) as i32)
-            != 0)
-    );
-    let mut buf2: [libc::c_char; 4] = [(0 as libc::c_char); 4];
-    {
-        let byte_0 = (buf2.as_mut_ptr() as *mut libc::c_char as *mut ::libc::c_void) as *mut u8;
-        for offset in 0..::std::mem::size_of::<[libc::c_char; 4]>() {
-            *byte_0.offset(offset as isize) = 0 as u8;
-        }
-        (buf2.as_mut_ptr() as *mut libc::c_char as *mut ::libc::c_void)
-    };
-    assert!(
-        ((((libc::read(
-            fds[(0) as usize],
-            (buf2.as_mut_ptr() as *mut libc::c_char as *mut ::libc::c_void),
-            ::std::mem::size_of::<[libc::c_char; 4]>()
-        )) == (2_isize)) as i32)
-            != 0)
-    );
-    assert!(
-        ((((libc::strcmp(
-            (buf2.as_mut_ptr()).cast_const(),
-            (c"ab".as_ptr().cast_mut()).cast_const()
-        )) == (0)) as i32)
-            != 0)
-    );
-    assert!(((((libc::close(fds[(1) as usize])) == (0)) as i32) != 0));
-    assert!(
-        ((((libc::read(
-            fds[(0) as usize],
-            (buf2.as_mut_ptr() as *mut libc::c_char as *mut ::libc::c_void),
-            ::std::mem::size_of::<[libc::c_char; 4]>()
-        )) == (0_isize)) as i32)
-            != 0)
-    );
-    assert!(((((libc::close(fds[(0) as usize])) == (0)) as i32) != 0));
-    let mut s: i32 = libc::socket(libc::AF_INET, libc::SOCK_STREAM, 0);
-    assert!(((((s) >= (0)) as i32) != 0));
-    assert!(((((libc::close(s)) == (0)) as i32) != 0));
     return 0;
 }
