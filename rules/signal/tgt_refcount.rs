7a79f24668190643658da2b26f5e9f11e8f6e9ea
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-08-07 12:06:31 +0100

    Add signal refcount rules

---
+++ rules/signal/tgt_refcount.rs
@@ -0,0 +1,24 @@
+// Copyright (c) 2022-present INESC-ID.
+// Distributed under the MIT license that can be found in the LICENSE file.
+
+use libcc2rs::*;
+
+fn f3(a0: i32, a1: FnPtr<fn(i32)>) -> FnPtr<fn(i32)> {
+    libcc2rs::signal_refcount(a0, a1.clone())
+}
+
+fn f2(a0: i32) -> i32 {
+    match nix::sys::signal::Signal::try_from(a0) {
+        Ok(__sig) => match nix::sys::signal::raise(__sig) {
+            Ok(()) => 0,
+            Err(__e) => {
+                libcc2rs::cpp2rust_errno().write(__e as i32);
+                -1
+            }
+        },
+        Err(__e) => {
+            libcc2rs::cpp2rust_errno().write(__e as i32);
+            -1
+        }
+    }
+}
