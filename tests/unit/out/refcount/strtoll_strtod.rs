f9d7cdeaaeec2993d5e3a3bb19503ed68812f1cc
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-08-08 15:31:50 +0100

    Update tests

--- tests/unit/out/refcount/strtoll_strtod.rs
+++ tests/unit/out/refcount/strtoll_strtod.rs
@@ -14,8 +14,12 @@ fn main_0() -> i32 {
     let s: Value<Ptr<u8>> = Rc::new(RefCell::new(Ptr::<u8>::null()));
     (*s.borrow_mut()) = Ptr::from_string_literal(b"42\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 10)
-            == 42_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 42_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 2_i64) as i32)
                 != 0)) as i32)
@@ -23,8 +27,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"  -17abc\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 10)
-            == (-17_i32 as i64)) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == (-17_i32 as i64)) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 5_i64) as i32)
                 != 0)) as i32)
@@ -32,16 +40,24 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"0xff\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 16)
-            == 255_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 16;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 255_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 4_i64) as i32)
                 != 0)) as i32)
             != 0)
     );
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 0)
-            == 255_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 0;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 255_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 4_i64) as i32)
                 != 0)) as i32)
@@ -49,8 +65,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"0755\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 0)
-            == 493_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 0;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 493_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 4_i64) as i32)
                 != 0)) as i32)
@@ -58,8 +78,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"0x\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 16)
-            == 0_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 16;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 0_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 1_i64) as i32)
                 != 0)) as i32)
@@ -67,8 +91,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"9223372036854775808\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 10)
-            == 9223372036854775807_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 9223372036854775807_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 19_i64) as i32)
                 != 0)) as i32)
@@ -76,8 +104,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"-9223372036854775809\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 10)
-            == (-9223372036854775807_i64 - 1_i64)) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == (-9223372036854775807_i64 - 1_i64)) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 20_i64) as i32)
                 != 0)) as i32)
@@ -85,8 +117,12 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"junk\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 10)
-            == 0_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 0_i64) as i32)
             != 0)
             && ((({
                 let _lhs = (*end.borrow()).clone();
@@ -97,25 +133,33 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"z\0");
     assert!(
-        ((((((libcc2rs::strtoll_refcount((*s.borrow()).clone(), (end.as_pointer()).clone(), 36)
-            == 35_i64) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            let __base = 36;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 35_i64) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 1_i64) as i32)
                 != 0)) as i32)
             != 0)
     );
     assert!(
-        (((libcc2rs::strtoll_refcount(
-            Ptr::from_string_literal(b"55\0").clone(),
-            Ptr::<Ptr::<u8>>::null().clone(),
-            10
-        ) == 55_i64) as i32)
+        ((({
+            let __nptr = Ptr::from_string_literal(b"55\0").clone();
+            let __endptr = Ptr::<Ptr<u8>>::null().clone();
+            let __base = 10;
+            libcc2rs::strtoll_refcount(__nptr, __endptr, __base)
+        } == 55_i64) as i32)
             != 0)
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"3.14\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == 3.1400000000000001E+0) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 3.1400000000000001E+0) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 4_i64) as i32)
                 != 0)) as i32)
@@ -123,8 +167,11 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"  -2.5e3xyz\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == -2.5E+3) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == -2.5E+3) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 8_i64) as i32)
                 != 0)) as i32)
@@ -132,8 +179,11 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"1.e5\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == 1.0E+5) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 1.0E+5) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 4_i64) as i32)
                 != 0)) as i32)
@@ -141,8 +191,11 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b".5\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == 5.0E-1) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 5.0E-1) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 2_i64) as i32)
                 != 0)) as i32)
@@ -150,8 +203,11 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"1e\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == 1.0E+0) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 1.0E+0) as i32)
             != 0)
             && ((((((*end.borrow()).clone() - (*s.borrow()).clone()) as i64) == 1_i64) as i32)
                 != 0)) as i32)
@@ -159,8 +215,11 @@ fn main_0() -> i32 {
     );
     (*s.borrow_mut()) = Ptr::from_string_literal(b"junk\0");
     assert!(
-        ((((((libcc2rs::strtod_refcount((*s.borrow()).clone(), (end.as_pointer()).clone())
-            == 0.0E+0) as i32)
+        (((((({
+            let __nptr = (*s.borrow()).clone();
+            let __endptr = (end.as_pointer()).clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 0.0E+0) as i32)
             != 0)
             && ((({
                 let _lhs = (*end.borrow()).clone();
@@ -170,10 +229,11 @@ fn main_0() -> i32 {
             != 0)
     );
     assert!(
-        (((libcc2rs::strtod_refcount(
-            Ptr::from_string_literal(b"+0.375e-1\0").clone(),
-            Ptr::<Ptr::<u8>>::null().clone()
-        ) == 3.7499999999999999E-2) as i32)
+        ((({
+            let __nptr = Ptr::from_string_literal(b"+0.375e-1\0").clone();
+            let __endptr = Ptr::<Ptr<u8>>::null().clone();
+            libcc2rs::strtod_refcount(__nptr, __endptr)
+        } == 3.7499999999999999E-2) as i32)
             != 0)
     );
     return 0;
